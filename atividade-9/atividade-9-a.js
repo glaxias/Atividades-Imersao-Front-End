627690aa8dcf55acc56c593fa23c724ef2c61041
Michelle Jennifer <michramalho@gmail.com>
2022-07-30 14:48:31 -0300

    adiciona atividade 9

---
+++ atividade-9/atividade-9-a.js
@@ -0,0 +1,25 @@
+// Atividade 9
+// Adicione a resposta na linha abaixo de cada exercício
+
+// 1 - Declare uma variável chamada `nome` que receba o seu nome.
+var nome = "Mich";
+
+// 2 - Declare uma variável chamada `idade` que receba a sua idade.
+var idade = 27;
+
+// 3 - Declare uma variável chamado `usuario` que receba um objeto contendo as propriedades `nome` e `idade`.
+var usuario = {
+    nome: "Mich",
+    idade: 27
+}
+
+// 4 - Declare uma variável chamada `usuarios` que receba um array contendo 4 números de 2 casas decimais.
+var usuarios = [42, 64, 90, 26];
+
+// 5 - Calcule o valor do comprimento da circuferência de um círculo com raio 5. A formulá é: C = 2πr
+// Utilize as variáveis abaixo para ajudar a criar a expressão.
+// var pi = 3.14;
+// var raio = 5;
+var raio = 5;
+var pi = 3.14;
+var circunferencia = 2 * pi * raio;
