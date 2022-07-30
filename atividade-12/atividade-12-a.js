82a084e3f0cfd8ce05cf5d4b22330adc3c1246e0
Michelle Jennifer <michramalho@gmail.com>
2022-07-30 14:50:31 -0300

    adiciona atividade 12

---
+++ atividade-12/atividade-12-a.js
@@ -0,0 +1,41 @@
+/*
+Atividade 12
+Adicione a resposta na linha abaixo de cada exercício
+Coloque todo o código do arquivo em uma IIFE.
+Em todos os retornos, use o console.log para imprimir o resultado.
+*/
+(function () {
+/*
+1 - Use o operador typeof para verificar o tipo de uma string, um número e um booleano.
+*/
+
+    typeof "Todas as Letras";
+
+    typeof 42;
+
+    typeof true;
+
+/*
+2 - Crie uma função que recebe o argumento `curso` que recebe uma string.
+Crie uma variável constante dentro da função chamada `escola` que recebe o valor "Todas as Letras".
+Retorne a string concatenada com a variável `escola`. Exemplo: "Curso de JavaScript - Todas as Letras".
+Use template strings para o retorno.
+*/
+
+    function recebeCurso(curso) {
+        const escola = "Todas as Letras";
+        return `${curso} - ${escola}`;
+    } 
+
+
+
+/*
+3 - Crie uma função que recebe o argumento `idade` que recebe um número
+e retorne uma string dizendo "Minha idade é `idade`". Use template strings para o retorno.
+*/
+
+function recebeIdade(idade) {
+    return `Minha idade é ${idade}`;
+}
+
+})();
